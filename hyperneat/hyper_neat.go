286a9b15d65b5cb25039b84cc21313f755a2debd
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2019-02-07 15:56:51 +0200

    The activators list now is in core NEAT library

--- hyperneat/hyper_neat.go
+++ hyperneat/hyper_neat.go
@@ -2,33 +2,27 @@
 package hyperneat
 
 import (
-	"github.com/yaricom/goNEAT/neat"
-	"github.com/yaricom/goNEAT/neat/network"
 	"io"
-	"github.com/spf13/viper"
-	"strings"
-	"strconv"
 	"errors"
 	"bytes"
+
+	"github.com/spf13/viper"
+	"github.com/yaricom/goNEAT/neat"
+	"github.com/yaricom/goNEAT/neat/utils"
 )
 
 // The HyperNEAT execution context
 type HyperNEATContext struct {
 	// The NEAT context included
-	NeatContext            *neat.NeatContext
+	NeatContext        *neat.NeatContext
 
 	// The threshold value to indicate which links should be included
-	LinkThershold          float64
+	LinkThershold      float64
 	// The weight range defines the minimum and maximum values for weights on substrate connections, they go from -WeightRange to +WeightRange, and can be any integer
-	WeightRange            float64
-
-	// The CPPN neuron nodes activation functions list to choose from
-	CPPNNodeActivators     []network.NodeActivationType
-	// The probabilities of CPPN node activator function selection
-	CPPNNodeActivatorsProb []float64
+	WeightRange        float64
 
 	// The substrate activation function
-	SubstrateActivator     network.NodeActivationType
+	SubstrateActivator utils.NodeActivationType
 }
 
 // Loads context from provided configuration data
@@ -48,24 +42,8 @@ func (h *HyperNEATContext) LoadContext(r io.Reader) error {
 
 	// read substrate activator
 	subAct := v.GetString("substrate_activator")
-	if h.SubstrateActivator, err = network.NodeActivators.ActivationTypeFromName(subAct); err != nil {
+	if h.SubstrateActivator, err = utils.NodeActivators.ActivationTypeFromName(subAct); err != nil {
 		return err
-	}
-
-	// read activation functions list
-	actFns := v.GetStringSlice("cppn_activators")
-	h.CPPNNodeActivators = make([]network.NodeActivationType, len(actFns))
-	h.CPPNNodeActivatorsProb = make([]float64, len(actFns))
-	for i, line := range actFns {
-		fields := strings.Fields(line)
-		if h.CPPNNodeActivators[i], err  = network.NodeActivators.ActivationTypeFromName(fields[0]); err != nil {
-			return err
-		}
-		if prob, err := strconv.ParseFloat(fields[1], 64); err != nil {
-			return err
-		} else {
-			h.CPPNNodeActivatorsProb[i] = prob
-		}
 	}
 
 	return nil
--- hyperneat/hyper_neat_test.go
+++ hyperneat/hyper_neat_test.go
@@ -2,10 +2,9 @@ package hyperneat
 
 import (
 	"testing"
-	"github.com/yaricom/goNEAT/neat/network"
 	"errors"
-	"fmt"
 	"os"
+	"github.com/yaricom/goNEAT/neat/utils"
 )
 
 func TestHyperNEATContext_LoadContext(t *testing.T) {
@@ -26,22 +25,7 @@ func TestHyperNEATContext_LoadContext(t *testing.T) {
 }
 
 func checkHyperNEATContext(context *HyperNEATContext) error {
-	if len(context.CPPNNodeActivators) != 4 {
-		return errors.New(fmt.Sprintf("len(context.CPPNNodeActivators) != 4, but: %d", len(context.CPPNNodeActivators)))
-	}
-	activators := []network.NodeActivationType{network.SigmoidBipolarActivation,
-		network.GaussianBipolarActivation, network.LinearAbsActivation, network.SineActivation}
-	probs := []float64{0.25, 0.35, 0.15, 0.25}
-	for i, a := range activators {
-		if context.CPPNNodeActivators[i] != a {
-			return errors.New(fmt.Sprintf("Wrong CPPN activator type at: %d", i))
-		}
-		if context.CPPNNodeActivatorsProb[i] != probs[i] {
-			return errors.New(fmt.Sprintf("Wrong CPPN activator probability at: %d", i))
-		}
-	}
-
-	if context.SubstrateActivator != network.SigmoidSteepenedActivation {
+	if context.SubstrateActivator != utils.SigmoidSteepenedActivation {
 		return errors.New("context.SubstrateActivator != network.SigmoidSteepenedActivation")
 	}
 	return nil
